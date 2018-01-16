03c3160607ed804b1f72b0b164237e1321e943d7
rio197 <rio197@Kedaung-PC>
2018-01-16 00:00:38 -0500

    Add main method

--- src/conditionalstatements/ConditionalStatements.java
+++ src/conditionalstatements/ConditionalStatements.java
@@ -16,6 +16,9 @@ public class ConditionalStatements {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+        int a = 5;
+        if (a > 1) {
+            System.out.println("I'm greater than 1");
+        }
+    }    
 }
